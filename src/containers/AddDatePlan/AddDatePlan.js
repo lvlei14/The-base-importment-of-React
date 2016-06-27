0443c58100f2acf9b9e8615a51c45b0cc3ff07b9
吕银蕾 <lvlei14@163.com>
2016-06-27 21:31:30 +0800

    date time input

--- src/containers/AddDatePlan/AddDatePlan.js
+++ src/containers/AddDatePlan/AddDatePlan.js
@@ -2,7 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import HeadNaviBar from '../../components/HeadNaviBar/HeadNaviBar';
 import { connect } from 'react-redux';
 import { showDiaglog } from '../../redux/modules/diaglog';
-const DateTimeField = require('react-bootstrap-datetimepicker');
+import DateTimeField from 'react-bootstrap-datetimepicker';
+// import moment from 'moment';
 import './DateTimePicker.scss';
 import { addDatePlan, loadTemplateItem } from '../../redux/modules/addDatePlan';
 
@@ -24,6 +25,15 @@ export default class AddDatePlan extends Component {
     msg: PropTypes.string,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputFormat: 'YYYY-MM-DD h:mm A',
+      startTime: new Date(),
+      endTime: new Date(),
+    };
+  }
+
   componentDidMount() {
     const templateId = this.props.routeParams.id;
     this.props.loadTemplateItem(templateId);
@@ -37,38 +47,29 @@ export default class AddDatePlan extends Component {
     }
   }
 
-  clickAddBtn() {
-    const template = this.props.template;
-    // const templateType = template[0].name;
-    const templateCon = template && template[0] && template[0].content;
-    const temItem = [];
-    const result = {};
-    for (const iKey in templateCon) {
-      if (!templateCon.hasOwnProperty(iKey)) continue;
-      const objItem = {
-        label: templateCon[iKey].label,
-        value: this.refs[templateCon[iKey].key].value
-      };
-      const obj = {};
-      obj[templateCon[iKey].key] = objItem;
-      temItem.push(obj);
-    }
-    const type = {
-      label: '日程类型',
-      value: this.refs.templateTypeRef.value
-    };
-    const isInner = {
-      label: '是否院内',
-      value: this.refs.localRef.value === '院内' ? true : false
-    };
-    result.content = temItem;
-    result.type = type;
-    result.is_inner = isInner;
-    result.template = template && template[0]._id;
-    result.start_time = this.refs.startTimeRef.value;
-    result.end_time = this.refs.endTimeRef.value;
-    console.log(result);
-    this.props.addDatePlan(result);
+  getNowFormatDate(day) {
+    const date = new Date(day);
+    const seperator1 = '-';
+    const seperator2 = ':';
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const strDate = date.getDate();
+    const currentdate = year + seperator1 + month + seperator1 + strDate + ' ' + date.getHours() + seperator2 + date.getMinutes() + seperator2 + date.getSeconds();
+    return currentdate;
+  }
+
+  handleChange = (newDate) => {
+    const newDateNew = new Date(parseInt(newDate));
+    this.setState({
+      startTime: newDateNew
+    });
+  }
+
+  handleChangeEndTime = (newDate) => {
+    const newDateNew = new Date(parseInt(newDate));
+    this.setState({
+      endTime: newDateNew
+    });
   }
 
   showFormType(formItem) {
@@ -112,11 +113,46 @@ export default class AddDatePlan extends Component {
     }
   }
 
+  clickAddBtn() {
+    const template = this.props.template;
+    // const templateType = template[0].name;
+    const templateCon = template && template[0] && template[0].content;
+    const temItem = [];
+    const result = {};
+    for (const iKey in templateCon) {
+      if (!templateCon.hasOwnProperty(iKey)) continue;
+      const objItem = {
+        label: templateCon[iKey].label,
+        value: this.refs[templateCon[iKey].key].value
+      };
+      const obj = {};
+      obj[templateCon[iKey].key] = objItem;
+      temItem.push(obj);
+    }
+    const type = {
+      label: '日程类型',
+      value: this.refs.templateTypeRef.value
+    };
+    const isInner = {
+      label: '是否院内',
+      value: this.refs.localRef.value === '院内' ? true : false
+    };
+    result.content = temItem;
+    result.type = type;
+    result.is_inner = isInner;
+    result.template = template && template[0] && template[0]._id;
+    result.start_time = this.getNowFormatDate(this.state.startTime);
+    result.end_time = this.getNowFormatDate(this.state.endTime);
+    console.log(result);
+    this.props.addDatePlan(result);
+  }
+
   render() {
     const template = this.props.template;
     const templateCon = template && template[0] && template[0].content;
     const templateType = template && template[0] && template[0].name;
     console.log(templateType);
+    const {inputFormat} = this.state;
     return (
       <div>
         <HeadNaviBar>添加日程</HeadNaviBar>
@@ -143,14 +179,20 @@ export default class AddDatePlan extends Component {
                 <label className={ styles.leftPlaceholder}>开始时间</label>
                 <div className={styles.scheduleType}>
                   <section className={styles.dateTimePicker}>
-                    <DateTimeField />
+                    <DateTimeField
+                      inputFormat={inputFormat}
+                      onChange={this.handleChange} />
                   </section>
                 </div>
               </li>
               <li>
                 <label className={ styles.leftPlaceholder}>结束时间</label>
                 <div className={styles.scheduleType}>
-                  <input ref="endTimeRef" type="date" />
+                  <section className={styles.dateTimePicker}>
+                    <DateTimeField
+                      inputFormat={inputFormat}
+                      onChange={this.handleChangeEndTime} />
+                  </section>
                 </div>
               </li>
               {
